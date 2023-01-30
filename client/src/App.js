4bd99704efab5227dcb25d97d57a79a2720d4624
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-01-30 17:27:24 +0300

    güncelleme
    güncelleme

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
 import React from 'react'
+import {useState} from 'react';
 import { Typography, AppBar } from '@material-ui/core';
 
 import VideoPlayer  from './components/VideoPlayer';
 import Notifications from './components/Notifications';
 import Options from './components/Options';
-const App = () => {
-    debugger;
+
+
+export default function App() {
+    const [count, setCount] = useState(0);
     return (
         <div>
-            {/* <AppBar position='static' color='inherit'>
-                <Typography variant="h2" align="center"> Video Chat
-                </Typography>
-            </AppBar> */}
-            {/* <VideoPlayer/>
-            <Options>
-                <Notifications/>
-            </Options> */}
+          <h1>Hello world</h1>
+          <AppBar>
+          </AppBar>
         </div>
-    )
+      );
 }
 
-export default App
+// const App = () => {
+//     debugger;
+//     return (
+//         <div>
+//             <AppBar  >
+
+//             </AppBar>
+//             {/* <AppBar position='static' color='inherit'>
+//                 <Typography variant="h2" align="center"> Video Chat
+//                 </Typography>
+//             </AppBar> */}
+//             {/* <VideoPlayer/>
+//             <Options>
+//                 <Notifications/>
+//             </Options> */}
+//         </div>
+//     )
+// }
+
+// export default App
